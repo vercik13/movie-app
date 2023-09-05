39fb7bf4d2c957f39ffc50d708efc7c4eab0c0d9
Veronika Vokounová <ver.cik@seznam.cz>
2023-09-05 21:32:31 +0200

    aktualizace

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { StarRating } from "./components/StarRating";
 import "./index.css";
 import App from "./App";
-
-const Test = () => {
-  return (
-    <>
-      <StarRating color="blue" maxRating={10} />
-      <p>This movies was rated X stars</p>
-    </>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/*<StarRating maxRating={5} />*/}
   </React.StrictMode>
 );
